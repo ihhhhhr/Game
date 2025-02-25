a624a2d579b42ee4258d20dec44d25347457bfe6
ihhhhhr <belinskiyigor2007@gmail.com>
2025-02-25 12:24:12 +0000

    gg

---
+++ Game1/code0.js
@@ -0,0 +1,20 @@
+gdjs.mainCode = {};
+gdjs.mainCode.localVariables = [];
+
+
+gdjs.mainCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.mainCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.mainCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['mainCode'] = gdjs.mainCode;
